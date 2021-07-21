953ee63df93ed2d3ebbcc7d0384a3dd7839f862d
MDM0764 <mishkath22@gmail.com>
2021-07-22 02:06:14 +0530

    fixed some issues

--- js/main.js
+++ js/main.js
@@ -8,4 +8,10 @@ displayNavbar = function () {
 
 $(window).on("load", function(){
      $(".loader-wrapper").fadeOut("slow");
-})
+});
+
+$(window).scroll(function() {
+     if($(".row timeline").is(":visible")){
+          console.log(2);
+     }
+});
